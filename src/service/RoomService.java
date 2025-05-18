e7d7dd622faceadab4d57702f5fba0ecddc71792
victormanin151 <victormanin151@gmail.com>
2025-05-18 19:49:18 +0300

    added RoomService

---
+++ src/service/RoomService.java
@@ -0,0 +1,27 @@
+package service;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import room.Room;
+
+import java.io.FileReader;
+import java.util.List;
+
+public class RoomService {
+    public static void viewRooms() {
+        try {
+            Gson gson = new Gson();
+            FileReader reader = new FileReader("rooms.json");
+
+            List<Room> rooms = gson.fromJson(reader, new TypeToken<List<Room>>() {}.getType());
+
+            System.out.println("Available Rooms:");
+            for (Room room : rooms) {
+                System.out.println(room.toString());
+            }
+
+            reader.close();
+        } catch (Exception e) {
+            System.out.println("Error reading rooms file: " + e.getMessage());
+        }
+    }
+}
