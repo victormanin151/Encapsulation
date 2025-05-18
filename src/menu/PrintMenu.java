12595a869f07b63b34fb0f4e3c7f2b57178ac058
victormanin151 <victormanin151@gmail.com>
2025-05-18 19:21:28 +0300

    added printmenu

---
+++ src/menu/PrintMenu.java
@@ -0,0 +1,16 @@
+package menu;
+
+public class PrintMenu{
+    public static void printMenu() {
+        System.out.println("\nWelcome to the Hotel Reservation System!");
+        System.out.println("Please choose an option:");
+        System.out.println("1. View Available Rooms");
+        System.out.println("2. Book a Room");
+        System.out.println("3. Cancel Booking");
+        System.out.println("4. Register");
+        System.out.println("5. Login");
+        System.out.println("6. Exit");
+        System.out.print("Your choice: ");
+    }
+}
+
